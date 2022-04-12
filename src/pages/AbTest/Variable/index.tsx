09b68db66941e3b490329e3a3bd71e90287d53a7
罗畅 <luochang@bilibili.com>
2022-04-12 11:02:30 +0800

    添加 导航吸顶功能

--- src/pages/AbTest/Variable/index.tsx
+++ src/pages/AbTest/Variable/index.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 import styles from './index.less';
 
 export default () => {
+  const [activeKey, setActiveKey] = useState(0);
+  const tabsData = ['商品信息', '配送信息', '商品详情'];
+  const changeTabs = (key: number) => {
+    console.log('key', key);
+    setActiveKey(key);
+  };
   return (
     <div className={styles['abtest-variable']}>
-      <h1 className={styles.title}>这是abtest-variable页面---</h1>
+      <div className={styles.body}>
+        <div className={styles.title}>hello world111</div>
+        <div className={styles['tabs-warpper']}>
+          {tabsData.map((item, key) => (
+            <div
+              key={key}
+              onClick={() => {
+                changeTabs(key);
+              }}
+              className={classNames(
+                styles.item,
+                activeKey === key ? styles.active : null,
+              )}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <div style={{ height: '200px' }}>ddddddddddd</div>
+        <div style={{ height: '200px' }}>ccccccccccc</div>
+        <div style={{ height: '200px' }}>hhhhhhhhhhh</div>
+      </div>
     </div>
   );
 };
